e0745dcb38b42f29cd6901ec26ef54f5c917de73
AkashChandran121233 <chandranakash92@gmail.com>
2025-01-05 16:27:40 +0530

    Addproduct.jsx

--- src/Pages/Addproduct.jsx
+++ src/Pages/Addproduct.jsx
@@ -18,7 +18,7 @@ export default function Addproduct() {
                 <input placeholder='Enter the category' />
             </div>
             <div>
-                <button style={{color:'black',,marginTop:'6px'}}>Add image</button>
+                <button style={{color:'black',marginTop:'6px'}}>Add image</button>
             </div>
             <div><button style={{color:'black',marginTop:'6px'}}>Add product</button></div>
         </form>
